55d3888a8c5d65a9da561dd60a6539193502dfe1
ravirajjadeja123 <ravirajjadeja7726@gmail.com>
2018-03-18 10:20:33 -0700

    Signed-off-by: ravirajjadeja123 <ravirajjadeja7726@gmail.com>

--- builder/includes/footer.php
+++ builder/includes/footer.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 2.3.7
+      <b></b>
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+    <strong>&nbsp;</strong>
   </footer>
