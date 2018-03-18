f1e1e2bdae398b1943426d63f74ef7457713fb1c
ravirajjadeja123 <ravirajjadeja7726@gmail.com>
2018-03-18 10:42:17 -0700

    Signed-off-by: ravirajjadeja123 <ravirajjadeja7726@gmail.com>

--- admin/project_builder1.php
+++ admin/project_builder1.php
@@ -50,7 +50,7 @@ readfile(PROJECT_STORAGE.$result->project_name.'.zip');*/
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Data Tables</title>
+  <title><?php echo SITE_NAME;?> | <?php echo $pagetitle; ?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
